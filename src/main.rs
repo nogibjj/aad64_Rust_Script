ddabaf495a0fec390828b10c8233bc2fc2e16d13
AaryaDesai1 <aad64@duke.edu>
2023-10-21 21:19:58 -0400

    Added resource code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use iris_functions::*;
-use std::error::Error as StdError;
 use std::time::Instant;
+use std::mem;
+use libc::{self, RUSAGE_SELF, timeval, rusage};
 
-fn main() -> Result<(), Box<dyn StdError>> {
-    // Measure start time
+fn get_resource_usage() -> rusage {
+    let mut usage: rusage = unsafe { mem::zeroed() };
+    let _ = unsafe { libc::getrusage(RUSAGE_SELF, &mut usage) };
+    usage
+}
+
+fn timeval_to_seconds(tv: timeval) -> f64 {
+    tv.tv_sec as f64 + (tv.tv_usec as f64 / 1_000_000.0)
+}
+
+fn main() {
     let start_time = Instant::now();
 
-    let data: Vec<IrisData> = read_iris_data("iris.csv")?;
+    let usage_start = get_resource_usage();
+
+    let data: Vec<IrisData> = read_iris_data("iris.csv").unwrap();
 
     let sepal_lengths: Vec<f64> = data.iter().map(|entry| entry.sepal_length).collect();
 
@@ -24,12 +36,16 @@ fn main() -> Result<(), Box<dyn StdError>> {
 
     visualize_data(&data, |entry| entry.sepal_length, |entry| entry.sepal_width);
 
-    // Measure end time
     let end_time = Instant::now();
-
-    // Calculate and print execution time
     let execution_time = end_time.duration_since(start_time);
-    println!("Execution time: {:?}", execution_time);
 
-    Ok(())
+    let usage_end = get_resource_usage();
+
+    let user_cpu_time = timeval_to_seconds(usage_end.ru_utime) - timeval_to_seconds(usage_start.ru_utime);
+    let system_cpu_time = timeval_to_seconds(usage_end.ru_stime) - timeval_to_seconds(usage_start.ru_stime);
+
+    println!("User CPU time: {:.6} seconds", user_cpu_time);
+    println!("System CPU time: {:.6} seconds", system_cpu_time);
+
+    println!("Execution time: {:?}", execution_time);
 }
